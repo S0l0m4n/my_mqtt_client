627189f4a81b9180db842c6bd713d63f36070a6d
Sam <sam@switchlat>
2019-09-30 12:22:42 +0100

    MyMqttClient: Use UUID for client id; do not auto-subscribe

--- MyMqttClient.py
+++ MyMqttClient.py
@@ -2,8 +2,12 @@
 
 from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
 import time
+import uuid
 
-MY_CLIENT_ID = "MyMqttClient"
+# Generate a random client id. I'm worried AWS IoT Core will kick this instance
+# of the client off the server if someone else also uses this script at the same
+# time.
+MY_CLIENT_ID = "MyMqttClient-{0}".format(uuid.uuid4().hex[:8])
 
 ROOTCA = "rootca.txt"
 CLICRT = "clicrt.txt"
@@ -11,23 +15,24 @@ CLIKEY = "clikey.txt"
 
 HOST = "ag8zxd6iafu8d-ats.iot.eu-west-1.amazonaws.com"
 PORT = 8883
-TOPIC = "telemetry"
 
 class MyMqttClient(AWSIoTMQTTClient):
-    def __init__(self, hubClientId):
+    def __init__(self):
         super().__init__(MY_CLIENT_ID)
 
         self.messages = []
-        self.topic = TOPIC + "/" + hubClientId
 
         self.configureEndpoint(HOST, PORT)
         self.configureCredentials(ROOTCA, CLIKEY, CLICRT)
         self.configureMQTTOperationTimeout(5)  # 5 sec
 
         self.connect()
+        print("{0} is connected to {1}".format(MY_CLIENT_ID, HOST))
 
-        if self.subscribe(self.topic, 1, self.subscribeCallback):
-            print('MyMqttClient is subscribed to "{}"'.format(self.topic))
+    def subscribe(self, topic):
+        if (super().subscribe(topic, 1, self.subscribeCallback)):
+            print('MyMqttClient is subscribed to "{}"'.format(topic))
+            self.topic = topic
         else:
             print("ERROR with MQTT client subscribing")
 
@@ -41,8 +46,9 @@ class MyMqttClient(AWSIoTMQTTClient):
         return len(self.messages)
 
 
-if __name__ == '__main__':
-    x = MyMqttClient("python")
+if __name__ == "__main__":
+    x = MyMqttClient()
+    x.subscribe("telemetry/modem")
     n = x.getNumMessages()
     while True:
         if n != x.getNumMessages():
