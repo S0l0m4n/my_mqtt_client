6b5727cf35883966e8624c020f5756d1f1ca76fe
Sam A <sam@stickerbrush>
2020-10-06 18:41:46 +0300

    my-mqtt-client: take host and creds as arguments

--- my-mqtt-client.py
+++ my-mqtt-client.py
@@ -7,26 +7,35 @@ from argparse import ArgumentParser, RawDescriptionHelpFormatter
 from MyMqttClient import MyMqttClient
 import time
 
+HOST_DEV = "ag8zxd6iafu8d-ats.iot.eu-west-1.amazonaws.com"
+HOST_PROD = "a1d18d1xa0vfcy-ats.iot.eu-west-1.amazonaws.com"
+
 parser = ArgumentParser(
     formatter_class=RawDescriptionHelpFormatter,
     description=
     """
     Subscribe to a topic on AWS IoT Core
 
-    Example: ./%(prog)s telemetry/unit1
+    Example: ./%(prog)s --host=dev --creds=dev command/unit1
 
-    Note: You must supply the following files in this directory:
+    NOTE: You must supply the following files in the "cred" directory:
       clicrt.txt    client certificate
       clikey.txt    client private key
       rootca.txt    Amazon CA root certificate
     """)
 
-parser.add_argument('topic', action='store', metavar='<topic>', type=str,
-    help='topic to subscribe to')
+parser.add_argument('--host', choices=['dev','prod'], help='host to connect to')
+parser.add_argument('--creds', metavar='<creds>', help='dir containing creds')
+parser.add_argument('topic', metavar='<topic>', help='topic to subscribe to')
 
 args = parser.parse_args()
 
-x = MyMqttClient()
+if args.host == "prod":
+    host = HOST_PROD
+else:
+    host = HOST_DEV
+
+x = MyMqttClient(host, creds=args.creds)
 x.subscribe(args.topic)
 n = x.getNumMessages()
 
